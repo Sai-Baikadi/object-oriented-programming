094842c2fd8fc7a1273fb9e0f3e3547de75f5ec8
Sai-Baikadi <bsaicharang@gmail.com>
2026-02-06 20:44:07 +0530

    feat:product program

--- object-oriented-programmimg/src/com/stschool/java/oop/Product.java
+++ object-oriented-programmimg/src/com/stschool/java/oop/Product.java
@@ -1,5 +1,19 @@
 package com.stschool.java.oop;
 
 public class Product {
+    int id=100;
+    String name;
+    double price;
+     Product( int id,String name, double price){
+         this.id=id;
+         this.name=name;
+         this.price=price;
+     }
+       void displayProductDetails(){
+         System.out.println("id:" + id);
+         System.out.println("name:" + name);
+         System.out.println("price:" + price);
+
+       }
 
 }
