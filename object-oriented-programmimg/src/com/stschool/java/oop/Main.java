a1913d32ce4b60968cd96620e9b01d98e59f1065
Sai-Baikadi <bsaicharang@gmail.com>
2026-02-06 20:43:47 +0530

    feat:product program

--- object-oriented-programmimg/src/com/stschool/java/oop/Main.java
+++ object-oriented-programmimg/src/com/stschool/java/oop/Main.java
@@ -1,4 +1,11 @@
 package com.stschool.java.oop;
 
-public class Main {
+ class Main {
+     public static void main(String[] args) {
+        Product product1 = new Product(222, " hp", 20000);
+        Product product2 = new Product(333, " dell", 40000);
+        product1.displayProductDetails();
+         product2.displayProductDetails();
+
+    }
 }
